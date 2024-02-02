23b2fc0ce2b47e99c70581efb0dc8e3d084a3a5c
mayor-creator <67986615+mayor-creator@users.noreply.github.com>
2024-02-02 13:09:18 -0500

    add default function to set image location source

--- app/index.js
+++ app/index.js
@@ -1,12 +1,22 @@
-let icons = document.querySelectorAll(".icons");
 let active = document.querySelectorAll(".active");
 let questions = document.querySelectorAll(".question");
 
+let setDefault = () => {
+	for (let index = 0; index < active.length; index++) {
+		active[index].style.display = "none";
+		// set the image location source
+		let imageId = active[index].previousElementSibling.lastElementChild;
+		imageId.src = "../assets/images/icon-plus.svg";
+	}
+};
+
 for (let i = 0; i < questions.length; i++) {
 	questions[i].addEventListener("click", function () {
+		setDefault();
+		//change the image location source
 		let image = questions[i].lastElementChild;
 		image.src = "../assets/images/icon-minus.svg";
 		let displayAnswer = questions[i].nextElementSibling;
-		displayAnswer.stylsetDefault();
+		displayAnswer.style.display = "block";
 	});
 }
